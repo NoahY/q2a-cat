2418c3b649d29f63e7522b335b66cebbbf4f1f7a
NoahY <noahy57@gmail.com>
2012-03-07 18:28:19 +0530

    limit affects redirect

--- qa-cat-layer.php
+++ qa-cat-layer.php
@@ -5,7 +5,7 @@
 		function doctype() {
 			qa_html_theme_base::doctype();
 			if($this->request == 'admin/categories') {
-				if( qa_clicked('categorize_plugin_save')) {
+				if(qa_clicked('categorize_plugin_save')) {
 					$cnt = 0;
 					while(qa_post_text('cat_'.$cnt.'_id')) {
 						$cnt2 = 1;
@@ -15,7 +15,10 @@
 						
 						$cnt++;
 					}
-					qa_redirect(qa_request(), array('recalc' => 1));
+					$uncat = qa_db_read_one_value(
+						qa_db_query_sub("SELECT COUNT(postid) FROM ^posts WHERE type='Q' AND categoryid IS NULL")
+					);
+					qa_redirect(qa_request(), (int)$uncat?array('recalc' => 1,'categorize' => 'true'):array('recalc' => 1));
 				}
 				else if(isset($this->content['form']['fields']['allow_no_category']['error']))
 					$this->content['form']['fields']['allow_no_category']['error'] = preg_replace('/HREF="[^"]*"/','HREF="'.qa_path_html(qa_request(),array('categorize'=>'true')).'"',$this->content['form']['fields']['allow_no_category']['error']);
